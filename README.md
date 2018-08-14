db9891f77b8d01dc87f917104c8db8b052438471
roopkt <roopkt@gmail.com>
2018-08-14 18:27:40 -0400

    fixing serve

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome to Webpack 4 example
+
+This is an starter app for webpack 4
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const { resolve } = require('path')
-module.exports = env => {
+module.exports = function () {
     return {
         entry: './js/app.js',
         output: {
             filename: 'bundle.js',
             path: resolve(__dirname, 'dist')
         },
-        context: resolve(__dirname, 'src'),
-        devServer: {
-            contentBase: resolve(__dirname, 'dist'),
-            compress: true,
-            port: 4210
-        }
+        context: resolve(__dirname, 'src')
     }
 }
