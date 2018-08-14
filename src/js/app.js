1e2864eff1130858415b8aa39ca4f5cff87afc77
roopkt <roopkt@gmail.com>
2018-08-14 18:34:16 -0400

    added htmlwebpack plugin

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,1 @@
-document.write('Welcome');
+document.write('<h1>Welcome to Webpack 4 starter kit</h1>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-const { resolve } = require('path')
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = function () {
     return {
         entry: './js/app.js',
@@ -6,6 +8,10 @@ module.exports = function () {
             filename: 'bundle.js',
             path: resolve(__dirname, 'dist')
         },
-        context: resolve(__dirname, 'src')
+        context: resolve(__dirname, 'src'),
+        plugins: [
+            /** Html Webpack plugin helps to create index.html file for dev server */
+            new HtmlWebpackPlugin()
+        ]
     }
 }
